10e197ed23902db5a0d5a06d7b6d6dc173116673
Felipe Bontempo <felipe.bontempo@mercadolivre.com>
2022-07-21 17:21:24 -0300

    pequenos ajustes

--- app2.js
+++ app2.js
@@ -11,37 +11,36 @@ let products = [
     { name: 'Monitor', price: 200, quantity: 3, colors: [] },
 ]
 
-// 1. Adicione um ID exclusivo a cada produto começando em 1.
+console.log("1. Adicione um ID exclusivo a cada produto começando em 1.");
 let id = 1;
 for (let index = 0; index < products.length; index++) {
     const element = products[index];
     element.id = id++;
 }
+products.forEach(element => {
+    console.log(JSON.stringify(element));
+});
 
-// 2. Imprima o nome de cada um dos produtos no console.
+console.log("2. Imprima o nome de cada um dos produtos no console.");
 products.forEach(element => {
     console.log("Nome: " + element.name);
 });
 
-// 3. Imprima no console o produto com o id 3.
+console.log("3. Imprima no console o produto com o id 3.");
 let produto3 = products.find(element => element.id == 3);
 console.log("O id 3, é ref ao obj: " + JSON.stringify(produto3));
-console.log("=============================");
 
-// 4. Imprima no console os produtos com a cor “black”.
+console.log("4. Imprima no console os produtos com a cor “black”.");
 let produtosBlacks = products.filter(element => element.colors.includes('black'));
 produtosBlacks.forEach(element => {
     console.log(JSON.stringify(element));;
 });
-console.log("=============================");
 
-// 5. Imprima no console os produtos que não possuem cor.
+console.log(" 5. Imprima no console os produtos que não possuem cor.");
 let produtosSemCores = products.filter(element => element.colors.length == 0);
 produtosSemCores.forEach(element => {
     console.log(JSON.stringify(element));
 });
-
-console.log("OXE");
 
 //push - adiciona item ao final do arry
 //pop - apapga o ultimo elemento
